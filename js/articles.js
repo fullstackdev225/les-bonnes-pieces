20ad240fd860afddb6a3232a8ab3f16086cd9592
Potey Charles <ppsjcfullstackdeveloper@gmail.com>
2025-07-06 15:20:27 +0000

    Affichage des piéces sur la page web

---
+++ js/articles.js
@@ -0,0 +1,43 @@
+//Getting articles from the api...
+const response = await fetch('http://localhost:8081/pieces');
+const articles = await response.json();
+
+// Displaying articles in the HTML
+function displayArticles(articles) {
+    for(let i = 0 ; articles.length > i ; i++) {
+        const article = articles[i];
+        const articleElement = document.createElement('article');
+
+        const articleImage = document.createElement('img');
+        articleImage.src = article.image;
+       
+        const articleTitle = document.createElement('h2');
+        articleTitle.textContent = article.nom; 
+
+        const articlePrice = document.createElement('p');
+        articlePrice.textContent = `${article.prix} €`;
+
+        const articleType = document.createElement('p');
+        articleType.textContent = article.categorie ?? ("Aucune catégorie");
+
+        const articleDescription = document.createElement('p');
+        articleDescription.textContent = article.description ?? ("Aucune description");
+
+        const articleAvailability = document.createElement('p');
+        articleAvailability.textContent = article.disponibilite ? "En stock" : "Rupture de stock";
+
+        const articlesLists = document.querySelector('.articles-lists');
+        articlesLists.appendChild(articleElement);
+
+        articleElement.appendChild(articleImage);
+        articleElement.appendChild(articleTitle);
+        articleElement.appendChild(articlePrice);
+        articleElement.appendChild(articleType);
+        articleElement.appendChild(articleDescription);
+        articleElement.appendChild(articleAvailability);
+
+    }
+}
+
+displayArticles(articles);
+
