0dd9e3498d5a523c941dc1788abaf1dbe0d4d77a
Potey Charles <ppsjcfullstackdeveloper@gmail.com>
2026-03-24 23:40:47 +0000

    Filtrage des pièces autos

--- js/pieces.js
+++ js/pieces.js
@@ -38,3 +38,35 @@ function displayPieces(pieces){
 }
 
 displayPieces(pieces);
+
+//filter pieces...
+const buttonOptique = document.getElementById("btn-optique");
+buttonOptique.addEventListener("click", () => {
+    const piecesOptique = pieces.filter(piece => piece.categorie === "Optiques");
+    document.querySelector('.articles-section').innerHTML = "";
+    displayPieces(piecesOptique);
+});
+
+const buttonFreinage = document.getElementById("btn-freinage");
+buttonFreinage.addEventListener("click", () => {
+    const piecesFreinage = pieces.filter(piece => piece.categorie === "Freinage");
+    document.querySelector('.articles-section').innerHTML = "";
+    displayPieces(piecesFreinage);
+});
+
+//sort pieces...
+const buttonIncreasePrice = document.getElementById("btn-prix-croissant");
+buttonIncreasePrice.addEventListener("click", () => {
+    const increasePrice = Array.from(pieces);
+    increasePrice.sort((a,b) => a.prix -b.prix);
+    document.querySelector(".articles-section").innerHTML = "";
+    displayPieces(increasePrice);
+});
+
+const buttonDecreasePrice = document.getElementById("btn-prix-decroissant");
+buttonDecreasePrice.addEventListener("click", () => {
+    const decreasePrice = Array.from(pieces);
+    decreasePrice.sort((a,b) => b.prix - a.prix);
+    document.querySelector(".articles-section").innerHTML = "";
+    displayPieces(decreasePrice);
+});
